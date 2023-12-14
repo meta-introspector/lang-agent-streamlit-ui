6f4229d0c65face92304b947adf43a43329cafff
Radamés Ajna <radames@users.noreply.huggingface.co>
2023-12-14 20:20:02 +0000

    Replace cache_resource with cache_data (#6)
    - Replace cache_resource with cache_data (c779e754510887b7754f348236c12f5632f3803f)
    
    
    Co-authored-by: Yuichiro Tachibana <whitphx@users.noreply.huggingface.co>

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache_resource
+@st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
