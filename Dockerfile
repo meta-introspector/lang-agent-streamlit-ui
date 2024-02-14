9e428ff10c153ab0fefed70d279ee70b77634ec4
mike dupont <mike.dupont@introspector.local>
2024-02-14 14:18:55 -0500

    run as root

--- Dockerfile
+++ Dockerfile
@@ -21,18 +21,16 @@ RUN apt-get update && xargs -r -a /app/packages.txt apt-get install -y && rm -rf
 RUN pip3 install --no-cache-dir -r /app/requirements.txt
 
 # User
-RUN useradd -m -u 1000 user
-RUN chmod -R 777 /data
-USER user
-ENV HOME /home/user
+#RUN useradd -m -u 1000 user
+#RUN chmod -R 777 /data
+#USER user
+ENV HOME /root
 ENV PATH $HOME/.local/bin:$PATH
 
 WORKDIR $HOME
 RUN mkdir app
 WORKDIR $HOME/app
 COPY . $HOME/app
-
-# make writable
 
 
 EXPOSE 8501
--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,4 @@
 sudo docker kill streamlit
-sudo docker rm streamlit 
-sudo docker run --name streamlit -v /home/mdupont/2024/02/12/streamlit-docker-lang-agent-introspector/:/home/user/app/ -p 8501:8501 h4ckermike/lang_agent_streamlit_with_data:meta-coq-utils-data-1-2024-02-12
+sudo docker rm streamlit
+# example for mounting source -v /home/mdupont/2024/02/12/streamlit-docker-lang-agent-introspector/:/home/user/app/
+sudo docker run --name streamlit  -p 8501:8501 h4ckermike/lang_agent_streamlit_with_data:meta-coq-utils-data-1-2024-02-12
